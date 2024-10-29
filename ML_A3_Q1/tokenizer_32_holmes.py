dc4e3a01196f379bfbe9f9bad29e62a66b92f6af
it'sme:) <23110229@iitgn.ac.in>
2024-10-29 18:48:30 +0530

    Update tokenizer_32_holmes.py

--- ML_A3_Q1/tokenizer_32_holmes.py
+++ ML_A3_Q1/tokenizer_32_holmes.py
@@ -1,13 +1,23 @@
 
+
+import os
 import pickle
+import streamlit as st
 
 def save_tokenizer(tokenizer, path='tokenizer_32_holmes.pkl'):
     # Save the tokenizer to a file
     with open(path, 'wb') as f:
         pickle.dump(tokenizer, f)
 
-def load_tokenizer(path=r"tokenizer_32_holmes.pkl"):
+def load_tokenizer(path=None):
     # Load the tokenizer from a file
-    with open(path, 'rb') as f:
-        tokenizer = pickle.load(f)
-    return tokenizer
+    path = path or os.path.join(os.path.dirname(_file_), 'tokenizer_32_holmes.pkl')
+    st.write("Loading tokenizer from:", path)
+    
+    if os.path.exists(path):
+        with open(path, 'rb') as f:
+            tokenizer = pickle.load(f)
+        return tokenizer
+    else:
+        st.error(f"Tokenizer file not found at path: {path}")
+        return None
