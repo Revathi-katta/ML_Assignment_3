61238979879943b56d55cedb477ed9905c0ed9b8
it'sme:) <23110229@iitgn.ac.in>
2024-10-29 17:03:09 +0530

    Update tokenizer_64.py

--- ML_A3_Q1/tokenizer_64.py
+++ ML_A3_Q1/tokenizer_64.py
@@ -1,12 +1,12 @@
 
 import pickle
 
-def save_tokenizer(tokenizer, path='C:/Users/katta/Downloads/tokenizer_64.pkl'):
+def save_tokenizer(tokenizer, path='tokenizer_64.pkl'):
     # Save the tokenizer to a file
     with open(path, 'wb') as f:
         pickle.dump(tokenizer, f)
 
-def load_tokenizer(path='C:/Users/katta/Downloads/tokenizer_64.pkl'):
+def load_tokenizer(path='tokenizer_64.pkl'):
     # Load the tokenizer from a file
     with open(path, 'rb') as f:
         tokenizer = pickle.load(f)
