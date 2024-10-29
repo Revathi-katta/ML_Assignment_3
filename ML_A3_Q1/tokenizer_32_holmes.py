2fb2407f2f1f2286bef6c48c3ad3dc9fd507f51d
it'sme:) <23110229@iitgn.ac.in>
2024-10-29 17:02:44 +0530

    Update tokenizer_32_holmes.py

--- ML_A3_Q1/tokenizer_32_holmes.py
+++ ML_A3_Q1/tokenizer_32_holmes.py
@@ -1,12 +1,12 @@
 
 import pickle
 
-def save_tokenizer(tokenizer, path='C/:Users/katta/Downloads/tokenizer_32_holmes.pkl'):
+def save_tokenizer(tokenizer, path='tokenizer_32_holmes.pkl'):
     # Save the tokenizer to a file
     with open(path, 'wb') as f:
         pickle.dump(tokenizer, f)
 
-def load_tokenizer(path=r"C:\Users\katta\Downloads\tokenizer_32_holmes.pkl"):
+def load_tokenizer(path=r"tokenizer_32_holmes.pkl"):
     # Load the tokenizer from a file
     with open(path, 'rb') as f:
         tokenizer = pickle.load(f)
