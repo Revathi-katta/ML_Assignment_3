186b9fe7d2882550ae5ab9aa58c0e70f64a89750
Revathi <143334310+Revathi-katta@users.noreply.github.com>
2024-10-29 20:21:49 +0530

    Update tokenizer_64_holmes.py

--- ML_A3_Q1/tokenizer_64_holmes.py
+++ ML_A3_Q1/tokenizer_64_holmes.py
@@ -1,19 +1,9 @@
 import pickle
-import gdown
-import os
+def save_tokenizer(tokenizer, path='ML_A3_Q1/tokenizer_64_holmes.pkl'):
+    with open(path, 'wb') as f:
+        pickle.dump(tokenizer, f)
 
-# Define the URL for the tokenizer file on Google Drive
-TOKENIZER_URL = "https://drive.google.com/file/d/1RpEo4AdjVbdJTGuD-NrFdd2Zud9lUu8F/view?usp=sharing"
-
-def download_tokenizer(path='tokenizer.pkl'):
-    """Download the tokenizer from Google Drive if it doesn't exist locally."""
-    if not os.path.exists(path):
-        gdown.download(TOKENIZER_URL, path, quiet=False)
-    return path
-
-def load_tokenizer(path='tokenizer.pkl'):
-    """Load the tokenizer from a file."""
-    download_tokenizer(path)  # Ensure the tokenizer file is downloaded
+def load_tokenizer(path='ML_A3_Q1/tokenizer_64_holmes.pkl'):
     with open(path, 'rb') as f:
         tokenizer = pickle.load(f)
     return tokenizer
