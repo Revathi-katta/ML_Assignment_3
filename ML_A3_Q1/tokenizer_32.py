14ea4be68acfe4f7566103163210114bdeb6e138
Revathi <143334310+Revathi-katta@users.noreply.github.com>
2024-10-29 19:10:36 +0530

    Update tokenizer_32.py

--- ML_A3_Q1/tokenizer_32.py
+++ ML_A3_Q1/tokenizer_32.py
@@ -1,22 +1,16 @@
-import json
+import os
 import pickle
 
-from keras.preprocessing.text import tokenizer_from_json
-
 def save_tokenizer(tokenizer, path='tokenizer_32.pkl'):
-    # Save the tokenizer to a file
+    """Save the tokenizer to a file."""
     with open(path, 'wb') as f:
         pickle.dump(tokenizer, f)
 
 def load_tokenizer(path='tokenizer_32.pkl'):
-    # Load the tokenizer from a file
-    with open(path, 'rb') as f:
-        tokenizer = pickle.load(f)
-    return tokenizer
-
-def load_tokenizer_json(path='tokenizer_32.json'):
-    # Load the tokenizer from a JSON file
-    with open(path) as f:
-        data = json.load(f)
-        tokenizer = tokenizer_from_json(data)
-    return tokenizer
+    """Load the tokenizer from a file."""
+    if os.path.exists(path):
+        with open(path, 'rb') as f:
+            tokenizer = pickle.load(f)
+        return tokenizer
+    else:
+        raise FileNotFoundError(f"Tokenizer file not found at path: {path}")
